1c388027599e0de6d44a2a23407d517e35970c2f
liangzhao <liang_zhao02@163.com>
2024-10-16 11:35:43 +0800

    update

---
+++ day02/01-基础示例.py
@@ -0,0 +1,22 @@
+# 导包
+from appium import webdriver
+
+# 启动参数
+desired_caps = dict()
+
+# 手机的平台类型
+desired_caps['platformName'] = 'Android'
+
+# 手机的系统版本
+desired_caps['platformVersion'] = '9.0'
+
+# 手机的名字
+desired_caps['deviceName'] = 'Android'
+
+# 程序的包名
+desired_caps['appPackage'] = 'com.android.settings'
+# 程序的界面名
+desired_caps['appActivity'] = '.Settings'
+
+# 连接驱动 + 连接手机 + 打开程序（手机的信息）
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", )
